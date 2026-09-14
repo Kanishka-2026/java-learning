c8c2721136542d134e1716e107e077915608469a
kanishka-2026 <kanish20210@gmail.com>
2026-09-14 23:53:20 +0530

    Add Sum program

---
+++ scanner sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Scanner sum {
+    public static void main(string[]args){
+        Scanner sc = new Scanner(System.in);
+    System.out.print("Enter First number:");
+    int a=sc.nextInt();
+System.out.println("Enter second number:");
+     int b=sc.nextInt();
+    System.out.println("Sum="=(a+b));
+sc.close();
+    }
+}
